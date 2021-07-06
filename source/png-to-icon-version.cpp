db9a955437570fcf7b0f01bd5c5735aacd2eff67
Grigore Stefan <g_stefan@yahoo.com>
2021-07-06 20:20:20 +0300

    update

--- source/png-to-icon-version.cpp
+++ source/png-to-icon-version.cpp
@@ -12,10 +12,10 @@
 namespace PngToIcon {
 	namespace Version {
 
-		static const char *version_ = "1.5.0";
-		static const char *build_ = "7";
-		static const char *versionWithBuild_ = "1.5.0.7";
-		static const char *datetime_ = "2021-03-15 12:30:21";
+		static const char *version_ = "1.6.0";
+		static const char *build_ = "8";
+		static const char *versionWithBuild_ = "1.6.0.8";
+		static const char *datetime_ = "2021-07-06 15:11:46";
 
 		const char *version() {
 			return version_;
--- source/png-to-icon-version.hpp
+++ source/png-to-icon-version.hpp
@@ -10,10 +10,10 @@
 #ifndef PNG_TO_ICON_VERSION_HPP
 #define PNG_TO_ICON_VERSION_HPP
 
-#define PNG_TO_ICON_VERSION_ABCD                1,5,0,7
-#define PNG_TO_ICON_VERSION_STR                 "1.5.0"
-#define PNG_TO_ICON_VERSION_STR_BUILD           "7"
-#define PNG_TO_ICON_VERSION_STR_DATETIME        "2021-03-15 12:30:21"
+#define PNG_TO_ICON_VERSION_ABCD                1,6,0,8
+#define PNG_TO_ICON_VERSION_STR                 "1.6.0"
+#define PNG_TO_ICON_VERSION_STR_BUILD           "8"
+#define PNG_TO_ICON_VERSION_STR_DATETIME        "2021-07-06 15:11:46"
 
 #ifndef XYO_RC
 
