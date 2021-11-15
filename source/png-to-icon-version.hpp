1cbb7676e3f86ebbe14280ec84ea7d723cb0963b
Grigore Stefan <g_stefan@yahoo.com>
2021-11-15 23:07:47 +0200

    Update

--- source/png-to-icon-version.hpp
+++ source/png-to-icon-version.hpp
@@ -10,10 +10,10 @@
 #ifndef PNG_TO_ICON_VERSION_HPP
 #define PNG_TO_ICON_VERSION_HPP
 
-#define PNG_TO_ICON_VERSION_ABCD                1,8,0,20
-#define PNG_TO_ICON_VERSION_STR                 "1.8.0"
-#define PNG_TO_ICON_VERSION_STR_BUILD           "20"
-#define PNG_TO_ICON_VERSION_STR_DATETIME        "2021-08-09 17:38:04"
+#define PNG_TO_ICON_VERSION_ABCD                1,9,0,21
+#define PNG_TO_ICON_VERSION_STR                 "1.9.0"
+#define PNG_TO_ICON_VERSION_STR_BUILD           "21"
+#define PNG_TO_ICON_VERSION_STR_DATETIME        "2021-11-15 16:51:49"
 
 #ifndef XYO_RC
 
